0db94f6b439a5444a5c7c6549d50a2a8429cfd3a
Ritouk <raphbenhams@gmail.com>
2024-05-22 23:12:05 +0300

    bash scripts ready, need to  add service for startup

--- launch_spot1.sh
+++ launch_spot1.sh
@@ -3,6 +3,7 @@
 #Sourcing ros2
 source /opt/ros/humble/setup.bash
 source ~/spot_ws/install/setup.bash
+source ~/spot_ws/install/setup.ps1
 
 
 echo "ROS2 env sourced"
@@ -14,4 +15,4 @@ ros2 launch spot_bringup spot_app.launch.py &
 sleep 1
 
 #Open terminal and excute the command inside:
-ros2 run spot_py_pkg controller_input 
+gnome-terminal -- bash -c "ros2 run spot_py_pkg controller_input; exec bash"
