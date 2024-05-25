80c3332bf7a2638cf720a269b9b14da912fec8b9
Ritouk <raphbenhams@gmail.com>
2024-05-25 16:29:04 +0300

    Added defines and new logic

---
+++ spot_py_pkg/spot_py_pkg/defines.py
@@ -0,0 +1,77 @@
+##-- DEBUG MODE (1 yes, 0 no)
+IS_CALIB = 1
+IS_POS_TEST = 0
+###------ Servo Nb
+NIU = 0 #NOT IN USE
+"""
+XYZZ
+X - L or R - Left or Right
+Y - F or R - Front or Back (rear but easier to read)
+ZZ - EL, IR, ER - Knee (Elbow but easier to read), Internal or External Rotation
+"""
+#Right Front Leg
+RFK = 1
+RFI = 3
+RFE = 5
+#Left Front Leg
+LFK = 2
+LFI = 4
+LFE = 6
+#Right Back Leg
+RBK = 15
+RBI = 13
+RBE = 11
+#Left Back Leg
+LBK = 14
+LBI = 12
+LBE = 10
+
+###-------- Servo data
+SERVO_ORDER = [NIU, RFK, LFK, RFI, LFI, RFE, LFE, NIU, NIU, NIU, LBE, RBE, LBI, RBI, LBK, RBK]
+# First Calibration, to assemble the servos 
+CALIB_POS   = [NIU,  90,  90,  90,  90,  90,  90, NIU, NIU, NIU,  90,  90,  90,  90,  90,  90]
+CALIB_CORR =  [NIU,  30, -30,   0,   0,   0,   0, NIU, NIU, NIU,   0,   0,   0,   0, -30,  30]
+# Second Calibration, software correction for assemblies errors
+
+
+# POS 0, 7, 8, 9 is not in use in the servo controller, so its replaced with a NIU 
+SERVO_NB    = [NIU,  1,   2,  3,  4,   5,  6,  NIU, NIU, NIU, 10, 11, 12,  13, 14, 15]
+
+# Absolute Positions
+SERVO_ORDER = [NIU, RFK, LFK, RFI, LFI, RFE, LFE, NIU, NIU, NIU, LBE, RBE, LBI, RBI, LBK, RBK]
+DEFAULT_POS = [NIU ,  0, 180,  90,  90,  90,  90, NIU, NIU, NIU,  90,  90,  90,  90, 180,   0] + CALIB_CORR
+LOW_POS     = [NIU ,  0, 180,  90,  90,  90,  90, NIU, NIU, NIU,  90,  90,  90,  90, 180,   0] + CALIB_CORR
+
+ALPHA = 45
+UP_POS      = DEFAULT_POS + ALPHA * [NIU, 1, -1, -1, 1, 0, 0, NIU, NIU, NIU, 0, 0, 1,-1, -1, 1]
+#Relative Positions 
+
+#This array is to test particular position, the servo will jump directly to the position, be careful! 
+TEST_POS    = [NIU,  30, 150,  30, 150, 100,  90, NIU, NIU, NIU, 100,  85,  90,  85, 170,   0] + CALIB_CORR
+#maximum and minimum of the position
+MIN_POS =     [NIU,  20,  20,  20,  20,  20,  20, NIU, NIU, NIU,  20,  20,  20,  20,  20,  20]
+MAX_POS =     [NIU, 160, 160, 160, 160, 160, 160, NIU, NIU, NIU, 160, 160, 160, 160, 160, 160]
+
+MIN_POS =     [NIU,   0,   0,   0,   0,   0,   0, NIU, NIU, NIU,   0,   0,   0,   0,   0,   0]
+MAX_POS =     [NIU, 180, 180, 180, 180, 180, 180, NIU, NIU, NIU, 180, 180, 180, 180, 180, 180]
+"""
+How to calibrate
+1. Mechanical Calbiration
+Use IS_CALIB = 1 to activate the calibration mode
+All Servos will get to 90 degrees
+Assemble the servos, trying to get as close to 90 degrees as possible.
+2. Software Calibration
+Use IS_POS_TEST = 1 to activate the calibration mode
+External Rotation should be 90 degrees
+Internal Rotation should be 90 degrees
+Knees should be as entered as possible, 0 for right and 180 left
+
+To correct errors, make changes in the CALIB_CORR array. 
+In default, it should have enough margin to avoid any critical errors.
+
+Change the values until you get the precision required
+
+Update Min and Max pos to avoid any undesired angles, maximum or minimum
+note: The calibration should of taken care of Left Knees Max and Right Knee min
+
+"""
--- spot_py_pkg/spot_py_pkg/spot_control_class.py
+++ spot_py_pkg/spot_py_pkg/spot_control_class.py
@@ -4,7 +4,8 @@ import numpy as np
 #from rclpy.node import Node
 
 from adafruit_servokit import ServoKit
-    
+from spot_py_pkg.defines import *
+from defines import *
 class SpotPosition():
     def __init__(self):
         #init Servo kit
@@ -14,10 +15,20 @@ class SpotPosition():
         self.servo_data_init()
         
         #intializing 
-        self.wake_up()
+        if (IS_CALIB):
+            print("Calibration Mode")
+            self.direct_pos(CALIB_POS)
+        elif (IS_POS_TEST):
+            print("Position Testing")
+            self.direct_pos(TEST_POS)
+            #self.pos_test_safe(self, 3)
+        else:
+            print("Waking up")
+            self.wake_up()
           
     def __del__(self):
-        self.sleep()
+        pass
+        #self.sleep()
                 
     def servokit_init(self):
         try:
@@ -40,22 +51,21 @@ class SpotPosition():
         self.servos_nb   = [ 1,   2,  3,  4,   5,  6,  7,  8,  9,  10, 11, 12,  13, 14, 15]
         
         #Default position, low position
-        self.current_pos = [15, 180, 80, 80, 100, 90, -1, -1, -1, 100, 85, 90, 85, 170,  0]
-        
+        self.current_pos = DEFAULT_POS
         #maximum and minimum of the position
-        self.min_pos = [ 15,  20,  20,  20,  20,  20, -1, -1, -1,  20,  20,  20,  20,  20,   0]
-        self.max_pos = [160, 180, 160, 160, 160, 160, -1, -1, -1, 160, 160, 160, 160, 170, 160]
+        self.min_pos = MIN_POS
+        self.max_pos = MAX_POS
             
     def change_current_pos(self, servos, new_pos):
         for i, servo_nb in enumerate(servos):
             #print(f"{i}, {servo_nb}, {new_pos[i]}")
-            self.current_pos[servo_nb-1] = new_pos[i]
+            self.current_pos[servo_nb] = new_pos[i]
         print(self.current_pos)
 
     def get_current_pos(self, servos):
         current_pos = np.zeros(len(servos))
         for i, servo_nb in enumerate(servos):
-            current_pos[i] = self.current_pos[servo_nb-1]
+            current_pos[i] = self.current_pos[servo_nb]
         #print(current_pos)
         return current_pos
     
@@ -66,19 +76,22 @@ class SpotPosition():
         counter = 0
         for i, servo_nb in enumerate(servos):
             #print(f"{i}, {servo_nb}, {new_pos[i]}")
-            if (new_pos[i] < self.min_pos[servo_nb-1] and 
-                new_pos[i] > self.max_pos[servo_nb-1]):
+            if (new_pos[i] < self.min_pos[servo_nb] and 
+                new_pos[i] > self.max_pos[servo_nb]):
                 print("position is out of boundaries, cancelling movement")
                 self.get_logger().debug("!!!Robot Control has been Started")
                 return 0
-            if (new_pos[i] == self.current_pos[servo_nb-1]):
+            if (new_pos[i] == self.current_pos[servo_nb]):
                 counter+=1
         if counter == len(new_pos):
             print("position is the same as current, skipping movement")
             return 0
         return 1
     
-    def controlled_motion(self, servos, new_positions, t):
+    def controlled_motion_abs(self, servos, pos_array, t):
+        new_positions = np.zeros(len((servos)))
+        for i, servo_nb in enumerate(servos):
+            new_positions[i] = pos_array[servo_nb]
         #checking validity of data
         position_valid = self.boundaries_check(servos, new_positions)
         if not position_valid:
@@ -106,54 +119,66 @@ class SpotPosition():
         
     def low(self, times):
         #shoulders
-        self.controlled_motion([5, 6, 10, 11], [100, 90, 100, 85], times)
+        self.controlled_motion_abs([RFE, LFE, LBE, RBE], 
+                               [DEFAULT_POS[RFE], DEFAULT_POS[LFE], DEFAULT_POS[LBE], DEFAULT_POS[RBE]]
+                               , times)
         #forearms
-        self.controlled_motion([ 3,  4, 12 , 13,  1,   2,  14, 15], 
-                               [80, 80, 90,  85, 15, 180, 170,  0], times)
+        self.controlled_motion_abs([RFI,  LFI, LBI , RBI,  RFK,   LFK,  LBK, RBK], 
+                               [ DEFAULT_POS[RFI], DEFAULT_POS[LFI], DEFAULT_POS[LBI], DEFAULT_POS[RBI], DEFAULT_POS[RFK], DEFAULT_POS[LFK], DEFAULT_POS[LBK], DEFAULT_POS[RBK]]
+                               , times)
         
     def get_up(self, times):
         #shoulders
-        self.controlled_motion([5, 6, 10, 11], [100, 90, 100, 85], times)
+        self.controlled_motion_abs([RFE, LFE, LBE, RBE], 
+                               [UP_POS[RFE], UP_POS[LFE], UP_POS[LBE], UP_POS[RBE]]
+                               , times)
         #forearms & arms
-        self.controlled_motion([ 1,  2,  3,  4,  10,  11,  12, 13, 14, 15], 
-                               [95, 90, 30, 150, 100, 85, 135, 40, 80, 90], times)
+        self.controlled_motion_abs([ RFK, LFK, RFI, LFI,
+                                    LBI, RBI, LBK, RBK], 
+                               [ UP_POS[RFK], UP_POS[LFK], UP_POS[RFI], UP_POS[LFI], 
+                                UP_POS[LBI], UP_POS[RBI], UP_POS[LBK], UP_POS[RBK]]
+                               , times)
+        
         
     def lie_down(self, times):
         #shoulders
-        self.controlled_motion([ 5,   6,  10, 11], 
-                               [100, 90, 100, 85], times)
+        self.controlled_motion_abs([RFE, LFE, LBE, RBE], 
+                               [DEFAULT_POS[RFE], DEFAULT_POS[LFE], DEFAULT_POS[LBE], DEFAULT_POS[RBE]]
+                               , times)
         #forearms
-        self.controlled_motion([  3,  4,  12 , 13,  1,   2,  14, 15], 
+        self.controlled_motion_abs([  RFI,  LFI,  LBI , RBI,  RFK,   LFK,  LBK, RBK], 
                                [100, 80, 80,  100, 15, 180, 170,  0], times)
 
         #shoulders
-        self.controlled_motion([ 5,   6,  10, 11], 
+        self.controlled_motion_abs([ RFE,   LFE,  LBE, RBE], 
                                [45, 140, 140, 40], times)
 
     def sit(self, times):
         #shoulders
-        self.controlled_motion([5, 6, 10, 11], [100, 90, 100, 85], times)
+        self.controlled_motion_abs([RFE, LFE, LBE, RBE], 
+                               [DEFAULT_POS[RFE], DEFAULT_POS[LFE], DEFAULT_POS[LBE], DEFAULT_POS[RBE]]
+                               , times)
         #forearms
-        self.controlled_motion([ 3,   4, 12 , 13,  1,  2,  14, 15], 
+        self.controlled_motion_abs([ RFI,   LFI, LBI , RBI,  RFE,  LFK,  LBK, RBK], 
                                [30, 150, 90,  85, 95, 90, 160,  0], times)
     
     def low_default(self):
         #shoulders
-        self.kit.servo[5].angle = 100
-        self.kit.servo[6].angle = 90
-        self.kit.servo[10].angle = 100
-        self.kit.servo[11].angle = 85
+        self.kit.servo[RFE].angle = DEFAULT_POS[RFE]
+        self.kit.servo[LFE].angle = DEFAULT_POS[LFE]
+        self.kit.servo[LBE].angle = DEFAULT_POS[LBE]
+        self.kit.servo[RBE].angle = DEFAULT_POS[RBE]
         time.sleep(1)
         #forearms
-        self.kit.servo[3].angle = 80
-        self.kit.servo[4].angle = 80
-        self.kit.servo[13].angle = 85
-        self.kit.servo[12].angle = 90
+        self.kit.servo[RFI].angle = DEFAULT_POS[RFI]
+        self.kit.servo[LFI].angle = DEFAULT_POS[LFI]
+        self.kit.servo[RBI].angle = DEFAULT_POS[RBI]
+        self.kit.servo[LBI].angle = DEFAULT_POS[LBI]
         #arms
-        self.kit.servo[1].angle = 15
-        self.kit.servo[2].angle = 180
-        self.kit.servo[15].angle = 0
-        self.kit.servo[14].angle = 170
+        self.kit.servo[RFK].angle = DEFAULT_POS[RFK]
+        self.kit.servo[LFK].angle = DEFAULT_POS[LFK]
+        self.kit.servo[RBK].angle = DEFAULT_POS[RBK]
+        self.kit.servo[LBK].angle = DEFAULT_POS[LBK]
 
         time.sleep(1)
         
@@ -164,13 +189,82 @@ class SpotPosition():
     def sleep(self):
         self.low(5)         
 
-
+    def calib(self):
+        #shoulders
+        self.kit.servo[RFE].angle = 90
+        self.kit.servo[LFE].angle = 90
+        self.kit.servo[RBE].angle = 90
+        self.kit.servo[LBE].angle = 90
+        time.sleep(1)
+        #forearms
+        self.kit.servo[RFI].angle = 90
+        self.kit.servo[LFI].angle = 90
+        self.kit.servo[RBI].angle = 90
+        self.kit.servo[LBI].angle = 90
+        time.sleep(1)
+        #arms
+        self.kit.servo[RFK].angle = 90
+        self.kit.servo[LFK].angle = 90
+        self.kit.servo[RBK].angle = 90
+        self.kit.servo[LBK].angle = 90
+    
+    def pos_test(self):
+        #shoulders
+        self.kit.servo[RFE].angle = TEST_POS[RFE]
+        self.kit.servo[LFE].angle = TEST_POS[LFE]
+        self.kit.servo[LBE].angle = TEST_POS[LBE]
+        self.kit.servo[RBE].angle = TEST_POS[RBE]
+        time.sleep(1)
+        #forearms
+        self.kit.servo[RFI].angle = TEST_POS[RFI]
+        self.kit.servo[LFI].angle = TEST_POS[LFI]
+        self.kit.servo[RBI].angle = TEST_POS[RBI]
+        self.kit.servo[LBI].angle = TEST_POS[LBI]
+        #arms
+        self.kit.servo[RFK].angle = TEST_POS[RFK]
+        self.kit.servo[LFK].angle = TEST_POS[LFK]
+        self.kit.servo[RBK].angle = TEST_POS[RBK]
+        self.kit.servo[LBK].angle = TEST_POS[LBK]
+        
+    def direct_pos(self, array):
+        for i in [RFE, LFE, RBE, LBE]:
+            #print(f"servo {i} gets the value {array[i]}")
+            self.kit.servo[i].angle = array[i]
+        time.sleep(1)
+        for i in [RFI, LFI, RBI, LBI, RFK, LFK, RBK, LBK]:
+            #print(f"servo {i} gets the value {array[i]}")
+            self.kit.servo[i].angle = array[i]
+        time.sleep(1)
+                
+        
+    def pos_test_safe(self, times):
+        # getting to safe pos
+        #self.low_default()
+        #moving to position
+        self.controlled_motion_abs([RFE, LFE, LBE, RBE], 
+                               [TEST_POS[RFE], TEST_POS[LFE], TEST_POS[LBE], TEST_POS[RBE]]
+                               , times)
+        #forearms
+        self.controlled_motion_abs([RFI, LFI, LBI, RBI, 
+                                RFK, LFK, LBK, RBK], 
+                               [TEST_POS[RFI], TEST_POS[LFI], TEST_POS[LBI], TEST_POS[RBI], 
+                                TEST_POS[RFK], TEST_POS[LFK], TEST_POS[LBK], TEST_POS[RBK]]
+                               , times)
+            
+    def angle_norm(self, servo, angle):
+    #not sure its worth the headeache, will see.
+        if (servo%0 !=0):
+            self.kit.servo[servo].angle = 180 - angle
+        else:
+            self.kit.servo[servo].angle = angle
+            
 def main(args=None):
     spot_pos = SpotPosition()
-    spot_pos.low()
-    spot_pos.get_up()
-    spot_pos.lie_down()
+    #spot_pos.low()
+    #spot_pos.get_up()
+    #spot_pos.lie_down()
 
 if __name__ == "__main__":
+    pass
     main()
     
