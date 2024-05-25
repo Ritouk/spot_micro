35e10a7b24d22f1a5623f497a1979cddf61b8c82
Ritouk <raphbenhams@gmail.com>
2024-05-25 17:09:31 +0300

    Added defines_local to gitignore

--- spot_py_pkg/spot_py_pkg/defines_local.py
+++ spot_py_pkg/spot_py_pkg/defines_local.py
@@ -3,4 +3,4 @@ This file is ignored by the git, so it can store local info that varies between 
 Most important: Servo calibration
 """
 NIU = 0
-CALIB_CORR =  [NIU,  30, -30,   0,   0,   0,   0, NIU, NIU, NIU,   0,   0,   0,   0, -30,  30]
+CALIB_CORR =  [NIU,  30, -30,   0,   0,   0,   1, NIU, NIU, NIU,   0,   0,   0,   0, -30,  30]
